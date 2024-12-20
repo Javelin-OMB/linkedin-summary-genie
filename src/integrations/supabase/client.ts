1ab857a574938e628d949243e365f68c22696cae
thinkingkoala <privalikhinanadia@gmail.com>
2024-12-20 17:05:26 +0100

    supabase account switch

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://oafrayzemkdwjxuqclks.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hZnJheXplbWtkd2p4dXFjbGtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM2MTI0MjksImV4cCI6MjA0OTE4ODQyOX0.g3hazl0_ZUO6fKFQsyf-uv7UsheXi3JaHJa6jXFeoo4";
+const SUPABASE_URL = "https://obqefwpwudsgrybytzcv.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9icWVmd3B3dWRzZ3J5Ynl0emN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ2ODg3NzksImV4cCI6MjA1MDI2NDc3OX0.jJBM9oF1PVYCG7YkiZ-m-kC810D8ETYH951Mg5tWlKU";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
